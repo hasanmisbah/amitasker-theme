84f8d5df7d718406df7fab1b85f387514f13d99e
Hasan Misbah <hasanmisbah01@gmail.com>
2019-02-16 04:57:48 +0600

    redux framework added, functions added to hook, sinplyfied Code

--- inc/functions.php
+++ inc/functions.php
@@ -1,3 +1,4 @@
 <?php
 //
-require_once('theme-functions/functions.php');
+    require_once('theme-functions/functions.php');
+    require_once('theme-functions/cssJsLoader.php');
---
+++ lib/options.php
@@ -0,0 +1,3 @@
+<?php
+    require_once 'redux-framework/ReduxCore/framework.php';
+    require_once 'redux-framework/ReduxCore/framework.php';
